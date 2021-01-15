21f664a12a596943aaddb3b565cc68c562cc8e90
vaytaliy <53967995+vaytaliy@users.noreply.github.com>
2021-01-16 02:14:39 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Pool, Client } = require('pg');
 
 const pool = new Pool({
     user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,       //set up these parameters manually on local db environment
     port: process.env.DATABASE_PORT
