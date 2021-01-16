373eec4cce4cfdc282c429d71613cfc973949c40
vaytaliy <53967995+vaytaliy@users.noreply.github.com>
2021-01-16 03:17:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,6 +46,16 @@ app.use(cookieSession({
     maxAge: 1000 * 60 * 24 * 10,
 }))
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 
