0f468e58222d8b77d2bab8b91263c2fe5a8266e1
vaytaliy <53967995+vaytaliy@users.noreply.github.com>
2021-01-16 12:32:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,16 +31,16 @@ require('dotenv').config();
 app.use(express.static('public'));
 
 //app.use('**', createProxyMiddleware({ target: allowedOrigin, changeOrigin: true }));
-//app.use(cors({ origin: allowedOrigin, credentials: true}));
+app.use(cors({ origin: allowedOrigin, credentials: true}));
 // app.use(express.urlencoded({
 //     extended: false
 // }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
